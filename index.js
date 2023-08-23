c66fee76b704c4d0acd65e80dee859d2a3570fd2
letami <julian.tam.le@web.de>
2023-08-23 16:27:33 +0200

    refactored note model definition

--- index.js
+++ index.js
@@ -1,19 +1,83 @@
-const mongoose = require('mongoose')
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const Note = require('./models/note')
 
-const url = `mongodb+srv://juliantamle:${password}@fulstackopen.di8dgjb.mongodb.net/noteApp?retryWrites=true&w=majority`
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
 
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
+app.use(express.json())
+app.use(cors())
+app.use(requestLogger)
+app.use(express.static('build'))
 
-const noteSchema = new mongoose.Schema({
-    content: String,
-    important: Boolean,
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
 })
-
-const Note = mongoose.model('Note', noteSchema)
 
 app.get('/api/notes', (request, response) => {
     Note.find({}).then(notes => {
         response.json(notes)
     })
 })
+
+app.get('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const note = notes.find(note => note.id === id)
+
+    if (note) {
+        response.json(note)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+    const id = Number(request.params.id)
+    notes = notes.filter(note => note.id !== id)
+
+    response.status(204).end()
+})
+
+const generateId = () => {
+    const maxId = notes.length > 0
+        ? Math.max(...notes.map(n => n.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.content) {
+        return response.status(400).json({
+            error: 'content missing'
+        })
+    }
+
+    const note = {
+        content: body.content,
+        important: body.important || false,
+        id: generateId(),
+    }
+
+    notes = notes.concat(note)
+
+    response.json(note)
+})
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({error: 'unknown endpoint'})
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
+app.listen(PORT)
+console.log(`Server running in port ${PORT}`)
